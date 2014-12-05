659ccfff88299f851a51afee1d3bbf43d2c4dc53
ed <ejbyne@gmail.com>
2014-12-05 10:25:23 +0000

    now working for all repos of a user

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,8 +1,6 @@
 githubUserSearch.controller('GitUserSearchController', function($scope, $resource) {
 
   var searchResource = $resource('https://api.github.com/search/users');
-  var repoResource = $resource('https://api.github.com/orgs/makersacademy/repos'); 
-  // /bookmark_manager/commits');
   var creds = {
       client_id: "d6dc6a59ba1cebdb9205",
       client_secret: 'eb1e63221b5d03abd382de7075d5622ddb94e2c0',
@@ -24,12 +22,14 @@ githubUserSearch.controller('GitUserSearchController', function($scope, $resourc
       var commits = 0;
 
       var setCommitCount = function() {
+      var username = $scope.searchTerm;
+      var repoResource = $resource('https://api.github.com/users/' + username + '/repos'); 
         repoResource.query(creds, function(result) {
           result.forEach(function(repo) {
             repos.push({ name: repo.name });
           })
           repos.forEach(function(item) {
-            var selectedRepo = $resource('https://api.github.com/repos/makersacademy/' + item.name + '/commits');
+            var selectedRepo = $resource('https://api.github.com/repos/' + username + '/' + item.name + '/commits');
             selectedRepo.query(creds, function(result) {
               item.totalCommits = result.length;
               item.userCommits = 0;
