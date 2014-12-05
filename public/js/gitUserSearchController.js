69e1002e19c41263775382a277654c7f9065a8e3
ed <ejbyne@gmail.com>
2014-12-05 16:11:46 +0000

    added css

--- public/js/gitUserSearchController.js
+++ public/js/gitUserSearchController.js
@@ -8,8 +8,10 @@ githubUserSearch.controller('GitUserSearchController', function($scope, $resourc
     };
 
   $scope.doSearch = function() {
-    $('#search_for').hide()
-
+    $('.profile_container').hide();
+    $('#search_message').hide();
+    $('#repo_header').hide();
+    $('.repo_container').hide();
     if ($scope.searchTerm != '') {
       $scope.searchResult = searchResource.get({
         q: $scope.searchTerm,
@@ -17,36 +19,38 @@ githubUserSearch.controller('GitUserSearchController', function($scope, $resourc
         client_secret: 'eb1e63221b5d03abd382de7075d5622ddb94e2c0',
         per_page: '100'
       });
-
-      var repos = [];
-      var commits = 0;
-
-      var setCommitCount = function() {
-      var username = $scope.searchTerm;
-      var repoResource = $resource('https://api.github.com/users/' + username + '/repos'); 
-        repoResource.query(creds, function(result) {
-          result.forEach(function(repo) {
-            repos.push({ name: repo.name });
-          })
-          repos.forEach(function(item) {
-            var selectedRepo = $resource('https://api.github.com/repos/' + username + '/' + item.name + '/commits');
-            selectedRepo.query(creds, function(result) {
-              item.totalCommits = result.length;
-              item.userCommits = 0;
-              result.forEach(function(commit) {
-                if (commit.committer.login === $scope.searchTerm) {
-                  item.userCommits = item.userCommits + 1;
-                }
-              })
-            })
-          })
-          $scope.repos = repos;
-        })
-      }
-
-      setCommitCount();
-      $('#search_for').show();
-      $('#repos').show();
+      $('#search_message').show();
+      $('.profile_container').show();
     }
   }
+
+  $scope.viewRepos = function() {
+    var repos = [];
+    var setCommitCount = function() {
+      var username = $scope.searchTerm;
+      var repoResource = $resource('https://api.github.com/users/' + username + '/repos'); 
+      repoResource.query(creds, function(result) {
+        result.forEach(function(repo) {
+          repos.push({ name: repo.name });
+        })
+        repos.forEach(function(item) {
+          var selectedRepo = $resource('https://api.github.com/repos/' + username + '/' + item.name + '/commits');
+          selectedRepo.query(creds, function(result) {
+            item.totalCommits = result.length;
+            item.userCommits = 0;
+            result.forEach(function(commit) {
+              if (commit.committer.login === $scope.searchTerm) {
+                item.userCommits = item.userCommits + 1;
+              }
+            })
+          })
+        })
+        $scope.repos = repos;
+      })
+    }
+    setCommitCount();
+    $('#repo_header').show();
+    $('.repo_container').show();
+  }
+
 });
