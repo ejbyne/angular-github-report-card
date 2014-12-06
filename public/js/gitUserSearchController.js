b22fc493b1dfbae067312c7c59e53907fc307abe
ed <ejbyne@gmail.com>
2014-12-06 20:30:30 +0000

    further refactoring

--- public/js/gitUserSearchController.js
+++ public/js/gitUserSearchController.js
@@ -1,21 +1,54 @@
 githubUserSearch.controller('GitUserSearchController', function($scope, $resource) {
 
   var searchResource = $resource('https://api.github.com/search/users');
-  var creds = {
-      client_id: "d6dc6a59ba1cebdb9205",
-      client_secret: 'eb1e63221b5d03abd382de7075d5622ddb94e2c0',
-      per_page: '100'
-    };
 
-  $scope.doSearch = function() {
+  var creds = { 
+    client_id: 'd6dc6a59ba1cebdb9205',
+    client_secret: 'eb1e63221b5d03abd382de7075d5622ddb94e2c0',
+    per_page: '100' }
+
+  // var params = { 
+  //   q: $scope.searchTerm,
+  //   client_id: 'd6dc6a59ba1cebdb9205',
+  //   client_secret: 'eb1e63221b5d03abd382de7075d5622ddb94e2c0',
+  //   per_page: '100' }
+
+  // var viewUser = function(userInfo) {
+  //   $scope.searchResult = userInfo;
+  //   $('#search_message').show();
+  //   $('.profile_container').show();
+  // }
+
+  // $scope.doSearch = function() {
+  //   hideInfo;
+  //   if ($scope.searchTerm != '') {
+  //     var searchResource = $resource('https://api.github.com/users/' + $scope.searchTerm);
+  //     searchResource.get(creds).$promise.then(function(result) { viewUser(result); });
+  //   }
+  // }
+
+  //   $scope.doSearch = function() {
+  //   hideInfo;
+  //   if ($scope.searchTerm != '') {
+  //     $scope.searchResult = searchResource.get(params);
+  //     $('#search_message').show();
+  //     $('.profile_container').show();
+  //   }
+  // }
+
+  var hideInfo = function() {
     $('.profile_container').hide();
     $('#search_message').hide();
     $('#repo_header').hide();
     $('.repo_container').hide();
+  }
+
+  $scope.doSearch = function() {
+    hideInfo;
     if ($scope.searchTerm != '') {
       $scope.searchResult = searchResource.get({
         q: $scope.searchTerm,
-        client_id: "d6dc6a59ba1cebdb9205",
+        client_id: 'd6dc6a59ba1cebdb9205',
         client_secret: 'eb1e63221b5d03abd382de7075d5622ddb94e2c0',
         per_page: '100'
       });
@@ -24,18 +57,11 @@ githubUserSearch.controller('GitUserSearchController', function($scope, $resourc
     }
   }
 
-  $scope.viewRepos = function() {
-    var repoResource = $resource('https://api.github.com/users/' + $scope.searchTerm + '/repos'); 
-    repoResource.query(creds).$promise.then(reposHandler); 
-  }
-
   var commitsReader = function(repo, commits) {
     repo.totalCommits = commits.length;
     repo.userCommits = 0;
     commits.forEach(function(commit) {
-      if (commit.committer.login === $scope.searchTerm) {
-        repo.userCommits = repo.userCommits + 1;
-      }
+      if (commit.committer.login === $scope.searchTerm) { repo.userCommits = repo.userCommits + 1; }
     });
   }
   
@@ -49,5 +75,9 @@ githubUserSearch.controller('GitUserSearchController', function($scope, $resourc
     $('.repo_container').show();
   }
 
+  $scope.viewRepos = function() {
+    var repoResource = $resource('https://api.github.com/users/' + $scope.searchTerm + '/repos'); 
+    repoResource.query(creds).$promise.then(reposHandler); 
+  }
 
 });
